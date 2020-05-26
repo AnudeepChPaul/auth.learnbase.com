78e19381196b56c903f5f1452bf62c748091c74e
Anudeep Chandra Paul <achandrapaul@manh.com>
2020-05-27 03:13:29 +0530

    Serverside redirection to /login page

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 // const assetPrefix = process.env.BUILDING_FOR_NOW ? "/login" : "";
 
 module.exports = {
-  env: {
-    AUTH_SUCCESS_URL: "https://google.com",
-  },
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      "/": { page: "/login" }
-    };
-  },
 };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,25 +1,38 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import App from 'next/app'
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "next/app";
+import Router from "next/router";
 
+export function redirectUser(ctx, location) {
+  if (ctx.req) {
+    ctx.res.writeHead(302, { Location: location });
+    ctx.res.end();
+  } else {
+    Router.push(location);
+  }
+}
 export default class MyApp extends App {
-  // static async getInitialProps({ Component, router, ctx }) {
-  //   const res = await api().get('/getAppProperties')
-  //   const data = JSON.parse(JSON.stringify(res.data))
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {};
 
-  //   return {
-  //     pageProps: {
-  //       appProps: data
-  //     }
-  //   }
-  // }
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    if (ctx.pathname === "/" || ctx.pathname === "/_error") {
+      redirectUser(ctx, "/login");
+      return { pageProps };
+    }
+
+    return { pageProps };
+  }
 
   render() {
-    const { Component, pageProps } = this.props
+    const { Component, pageProps } = this.props;
 
     return (
       // <MainLayout {...pageProps.appProps}>
-        <Component {...pageProps}></Component>
+      <Component {...pageProps}></Component>
       // </MainLayout>
-    )
+    );
   }
 }
