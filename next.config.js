b89d5ec408b961148f5fb2635c9d40fdbaed99b1
Anudeep Chandra Paul <achandrapaul@manh.com>
2020-05-27 01:05:18 +0530

    Vercel Integration

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
-const assetPrefix = process.env.BUILDING_FOR_NOW ? "/login" : "";
+// const assetPrefix = process.env.BUILDING_FOR_NOW ? "/login" : "";
 
 module.exports = {
-  assetPrefix,
-  env: {
-    ASSET_PREFIX: assetPrefix,
+  assetPrefix
+  , env: {
+    AUTH_SUCCESS_URL: 'https://google.com'
   }
-  // distDir: "build",
-  // exportPathMap: async function (
-  //   defaultPathMap,
-  //   { dev, dir, outDir, distDir, buildId }
-  // ) {
-  //   return {
-  //     "/": { page: "/" },
-  //   };
-  // },
 };
